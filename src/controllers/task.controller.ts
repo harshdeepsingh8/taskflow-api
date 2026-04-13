81a56a508446a2963d73d11ae23fe7fc69e582ec
Harshdeep <haeshdeepsingh8@rrc.ca>
2026-04-12 22:25:41 -0500

    feat: add basic error handling to CRUD

--- src/controllers/task.controller.ts
+++ src/controllers/task.controller.ts
@@ -6,16 +6,26 @@ export const createTask = (req: Request, res: Response) => {
   res.json(task);
 };
 
-export const getTasks = (req: Request, res: Response) => {
-  res.json(service.getTasks());
+export const getTaskById = (req: Request, res: Response) => {
+  const task = service.getTaskById(req.params.id as string);
+  if (!task) return res.status(404).json({ message: "Not found" });
+  res.json(task);
 };
+export const getTasks = (req: Request, res: Response) => {
+  const tasks = service.getTasks();
+  res.json(tasks);
+}
 
 export const updateTask = (req: Request, res: Response) => {
   const updated = service.updateTask(req.params.id as string, req.body);
+  if (!updated) return res.status(404).json({ message: "Not found" });
   res.json(updated);
 };
 
 export const deleteTask = (req: Request, res: Response) => {
+  const task = service.getTaskById(req.params.id as string);
+  if (!task) return res.status(404).json({ message: "Not found" });
+
   service.deleteTask(req.params.id as string);
   res.json({ message: "Deleted" });
 };
